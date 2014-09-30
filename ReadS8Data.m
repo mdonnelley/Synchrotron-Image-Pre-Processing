81acb81ef8c98213a68b0defced930c77955109e
mdonnelley <martin.donnelley@gmail.com>
2014-09-30 16:09:21 +0930

    Change to read XLSX rather then CSV format

--- ReadS8Data.m
+++ ReadS8Data.m
@@ -15,26 +15,52 @@ function info = ReadS8Data(data)
 %******************************************************
 %
 
+% %% CSV
+% 
+% % Read the data file
+% fid = fopen(data);
+% file = textscan(fid, '%s %s %f %f %s %s %s %f %f %s %s %s %f %f %s', 'Delimiter', ',');
+% fclose(fid);
+% 
+% % Convert to a matlab cell array
+% info.image = file{1};
+% info.imagestart = file{2};
+% info.imagegofrom = file{3};
+% info.imagegoto = file{4};
+% info.imageformat = file{5};
+% 
+% info.flat = file{6};
+% info.flatstart = file{7};
+% info.flatgofrom = file{8};
+% info.flatgoto = file{9};
+% info.flatformat = file{10};
+% 
+% info.dark = file{11};
+% info.darkstart = file{12};
+% info.darkgofrom = file{13};
+% info.darkgoto = file{14};
+% info.darkformat = file{15};
+
+%% XLSX
+
 % Read the data file
-fid = fopen(data);
-file = textscan(fid, '%s %s %f %f %s %s %s %f %f %s %s %s %f %f %s', 'Delimiter', ',');
-fclose(fid);
+[num,txt]=xlsread(data,'','','basic');
 
 % Convert to a matlab cell array
-info.image = file{1};
-info.imagestart = file{2};
-info.imagegofrom = file{3};
-info.imagegoto = file{4};
-info.imageformat = file{5};
+info.image = txt(:,1);
+info.imagestart = txt(:,2);
+info.imagegofrom = num(:,1);
+info.imagegoto = num(:,2);
+info.imageformat = txt(:,5);
 
-info.flat = file{6};
-info.flatstart = file{7};
-info.flatgofrom = file{8};
-info.flatgoto = file{9};
-info.flatformat = file{10};
+info.flat = txt(:,6);
+info.flatstart = txt(:,7);
+info.flatgofrom = num(:,6);
+info.flatgoto = num(:,7);
+info.flatformat = txt(:,10);
 
-info.dark = file{11};
-info.darkstart = file{12};
-info.darkgofrom = file{13};
-info.darkgoto = file{14};
-info.darkformat = file{15};
+info.dark = txt(:,11);
+info.darkstart = txt(:,12);
+info.darkgofrom = num(:,11);
+info.darkgoto = num(:,12);
+info.darkformat = txt(:,15);
