0555415f7328674f020c9229bd42652ed1b19ee1
mdonnelley <martin.donnelley@gmail.com>
2017-05-31 10:54:32 +0930

    Ability to read XLS without F&D info
    Added the ability to read XLS info sheets that do not contain flat or
    dark files. Required for MuCLS data.

--- ReadS8Data.m
+++ ReadS8Data.m
@@ -2,7 +2,7 @@ function info = ReadS8Data(data)
 
 %
 %**********************************************************
-% Read data information from an CSV file
+% Read data information from an XLSX / CSV file
 %
 % written by: Martin Donnelley
 % date: 16/04/2010
@@ -44,26 +44,32 @@ function info = ReadS8Data(data)
 %% XLSX
 
 % Read the data file
-[num,txt] = xlsread(data,'','','basic');
+[num,txt,raw] = xlsread(data,'','','basic');
 
 % Convert to a matlab cell array
-info.image = txt(:,1);
-info.imagestart = txt(:,2);
-info.imagegofrom = num(:,1);
-info.imagegoto = num(:,2);
-info.imageformat = txt(:,5);
+if size(raw,2) >= 5,
+    info.image = txt(:,1);
+    info.imagestart = txt(:,2);
+    info.imagegofrom = num(:,1);
+    info.imagegoto = num(:,2);
+    info.imageformat = txt(:,5);
+end
 
-info.flat = txt(:,6);
-info.flatstart = txt(:,7);
-info.flatgofrom = num(:,6);
-info.flatgoto = num(:,7);
-info.flatformat = txt(:,10);
+if size(raw,2) >= 10,
+    info.flat = txt(:,6);
+    info.flatstart = txt(:,7);
+    info.flatgofrom = num(:,6);
+    info.flatgoto = num(:,7);
+    info.flatformat = txt(:,10);
+end
 
-info.dark = txt(:,11);
-info.darkstart = txt(:,12);
-info.darkgofrom = num(:,11);
-info.darkgoto = num(:,12);
-info.darkformat = txt(:,15);
+if size(raw,2) >= 15,
+    info.dark = txt(:,11);
+    info.darkstart = txt(:,12);
+    info.darkgofrom = num(:,11);
+    info.darkgoto = num(:,12);
+    info.darkformat = txt(:,15);
+end
 
 % Added for AS-2016-2 data to identify the first image in the breath
 if size(num,2) == 14, info.startimage = num(:,14); end
